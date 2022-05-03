b6f639789e90f5be61b7e400d7f8ada1dad91bda
deecooper <deecoopermedia@gmail.com>
2022-05-03 15:08:07 +0000

    Add the function that holds the game hangman and add in main function to call other functions

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1>Hangman World</h1>
+Hanman World is a word guessing game. As the name suggest the words that will be included in this game are taken from places all around the world. It is a python terminal game. The aim of the game is to guess the secret word before all of your guesses run out. 
+
+<h2>How to play</h2>
+
+If the user selects Play Game they will be asked to enter their name. The user will then be presented with blank spaces which represent the secret word the user has to guess. Each time the user guesses a wrong letter they are one step closer to being hanged which will be represented by the hangman icons. Each time the user guesses a correct letter it will replace the underscore with the correct letter. 
+
+example:
+Secret word: CANADA
+_ _ _ _ _
+user guesses: A
+_ A _ A _ A
+
+<h2> Features </h2>
+The user will be present with four options at the start of the game
+1. Play Game
+2. Instructions
+3. High Scores
+4. Exit
+
+
+
--- run.py
+++ run.py
@@ -24,45 +24,189 @@ data = high_scores.get_all_values()
 
 def get_word():
     """
-    This function gets the random word from the words.txt file"""
+    This function gets the random word from the words.txt file
+    """
     with open("words.txt", "r") as f:
         word = f.readlines()
     word_choice = random.choice(word)
-    return word_choice.upper()
-    #random_word = get_word()
-    #print(random_word)
+    return word_choice.upper
 
 def hangman_word(word_choice):
     #https://www.youtube.com/watch?v=m4nEnsavl6w
     """
     This function return the word as underscores for the user to guess the word
     """
-    word_completion = "_" + len(word_choice)
+    word_completion = "_" * len(word_choice)
     guessed = False
     letters_guessed = []
     words_guessed = []
+    guessmade = ''
     tries = 8
-    print(tries)
+    print("Lets Play")
+    print("----------------------")
     print(word_completion)
+    print("-----------------------")
+    print(hangman_stages(tries))
+    #https://www.youtube.com/watch?v=WZZ9pY-cP2s
+    while not guessed and tries > 0:
+        guess = input("please guess a letter or word").upper()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in letters_guessed:
+                print(f"You have already guessed this letter + {guess}")
+            elif guess not in word_choice:
+                print(f"This letter is not in the word {guess}")
+                tries -= 1
+                letters_guessed.append(guess)
+            else:
+                print(f"Well done {guess} is in the word")
+                letters_guessed.append(guess)
+                word_list = list(word_completion)
+                indices = [i for i, letter in enumerate(word_choice) if letter == guess]
+                for index in indices:
+                    word_list[index] = guess
+                word_completion = "".join(word_list)
+                if "_" not in word_completion:
+                    guessed = True
+        elif len(guess) == len(word_choice) and guess.isalpha():
+            if guess in words_guessed:
+                print(f"You already guessed the word {guess}")
+            elif guess != word_choice:
+                print(f"{guess} is not in the word")
+                tries -= 1
+                words_guessed.append(guess)
+            else:
+                guessed = True
+                word_completion = word_choice
+        else:
+            print("Guess is not valid try again")
+            print(hangman_stages(tries))
+            print(word_completion)
+            print("\n")
+    if guessed:
+        print("Well done you guessed the word")
+    else:
+        print(f"sorry you ran out of tries the word was {word_choice}")
+
+
+def hangman_stages(tries):
+    hangman_icons = ['''
+      -------
+      |     |
+      |     |
+      |     O
+      |    \|/
+      |    / \\
+      |
+    ----- ''' , '''
+      -------
+      |     |
+      |     |
+      |     O
+      |    \|/
+      |    /
+      |
+    ----- ''' , '''
+    
+      -------
+      |     |
+      |     |
+      |     O
+      |    \|/
+      |
+      |
+    ----- ''' , '''
+
+      -------
+      |     |
+      |     |
+      |     O
+      |     |/
+      |
+      |
+    ----- ''' , '''
+      -------
+      |     |
+      |     |
+      |     O
+      |     |
+      |
+      |
+    ----- ''', '''
+      -------
+      |     |
+      |     |
+      |     O
+      |
+      |
+      |
+    ----- ''' , '''
+      -------
+      |     |
+      |     |
+      |
+      |
+      |
+      |
+    ----- ''' , '''
+      ------- 
+      |     |
+      |
+      |
+      |
+      |
+      |
+    ----- ''', '''
+      ------- 
+      |     
+      |
+      |
+      |
+      |
+      |
+    ----- '''
+    ]
+    return hangman_icons(tries)
+    
+
+
+
+    
+
+"""
 
 def play_game():
-    name = input("input your name:  ")
-    print(f"Hello {name} welcome to hangman world")
-    get_word()
+    name = input("Enter your name:  ")
+    print(f"Hello {name} welcome to Hangman World")
+    print("----------------------------")
+
+"""
+    
+    #try statement here for strings
+
+def main():
+    word_choice = get_word()
+    hangman_word(word_choice)
+
+main()
 
 
+"""
+play_game()
 
+"""
+"""
 def welcome():
-    """
+    
     This function gives the user the option to play the game,
     read instructions, look at the high scores and exit the game
-    """
+    
     while True:
         welcome_msg = "Hello and welcome to Hangman\
         World please select one of the following \
-        options by typing in the number beside your\
-        selection into the answer field" 
+        options by typing a number. Type 1 for play game, \
+        2 for instructions, 3 for High Scores and 4 for exit " 
         print(welcome_msg)
+        print("_______________________________________")
         play = "1. Play Game"
         print(play)
         instructions = "2. Instructions"
@@ -72,29 +216,30 @@ def welcome():
         quit ="4. Quit"
         print(quit)
         start_choice = input("Enter your answer here 1,2,3,4: ")
+        
         if start_choice == "1":
-            
             print("You have choose play game")
             play_game()
             break
         elif start_choice == "2":
     
             print("Hangman World is a word guessing game and as the\
-            name suggests the user will be asked to guess the name of the\
-            place in the world which the computer has chosen")
+            name suggests the player will be asked to guess the name of the\
+            place in the world which the computer has chosen. The player will\
+            have eight attempts to guess the word before they are hanged. Good Luck!")
         elif start_choice == "3":
             print(data)
         elif start_choice == "4":
             print("quitting game bye")
+            sys(exit)
         else:
             print("you have not choose one of the selections\
             please try again")
+
+            #try statement for anything that isnt 1,2,3,4 and isnt a number
 
     
 
 welcome()
 
-
-
-
-    
+"""
